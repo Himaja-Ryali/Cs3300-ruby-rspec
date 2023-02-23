f0dddec0d5a2f0b8ae32b20b40ff78f6607940c9
Himaja Ryali <ryali.hr@gmail.com>
2023-02-23 23:39:22 +0000

    Added comments

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,18 +2,24 @@
 
 # Part 1
 
-def sum arr
-  sum = 0;
-  array.each { |a| sum+=a }
-  return sum;
+#defining the method to sum the elements of an array!
+
+def sum(array)                     # "def sum"is a function for defining a method.
+  sum = 0;		                  	 #initializing sum to zero
+  array.each { |a| sum+=a }        #logic : addition to the element of array.
+  return sum;			                 #returning the sum
 end
 
-def max_2_sum arr
-  if arr.length == 0
-    return 0
-    elsif arr.length == 1
-    return arr[0]
-    end
+
+# defining the method of summing the two largest elements in an array.
+
+def max_2_sum arr			
+  if arr.length == 0		           # it is initialiazing the array length to zero
+  return 0				                 # returning the zero if the array length is zero
+  elsif arr.length == 1		         # it is initialiazing the array length to one
+  return arr[0]		               	 #returning the first element of the array if length of array is one.
+  end
+  arr=arr.sort			               #it sorts the array in ascending order.
 end
 
 def sum_to_n? arr, n
@@ -22,22 +28,29 @@ end
 
 # Part 2
 
-def hello(name)
-  return "Hello, " + name
+def hello(name)		              	#defining a method hello and the parameter is name.
+  return "Hello, " + name       	#it retuns Hello and user given name as input
 end
 
-def starts_with_consonant? s
-  return false if !s or s.length == 0
-end
+def starts_with_consonant?(s)		           	#defining the method starts with consonant and parameter is S
+  return false if !s or s.length == 0	      #it retuns false if length of the string is zero
+  return !/\A[aeiou\W\d].*/i.match(s)	      #it returns true if the word is a consonant.
+  end
 
-def binary_multiple_of_4? s
-  return true if (/\d+/ =~ s) == 0
+  def binary_multiple_of_4?(s)					#defining method binary multiple of 4 and parameter is S
+    return true if (/\d+/ =~ s) == 0   	#returns true if the dividend is not same as divisor and the remainder is zero then it is multiple of 4
     return false
-end
+    end
 
 # Part 3
 
-class BookInStock
-  attr_accessor :isbn, :price
+class BookInStock							                               #the name of the class is book in stock
+
+  attr_accessor :isbn, :price					                       #assigning attributes to ISBN and PRICE
   
-end
+  def initialize isbn, price				      	                 #initializing the attributes to the method		
+  raise ArgumentError if isbn.empty? || price <= 0	         #If isbn is empty and the price is in negative then it raised argument error
+  @isbn = isbn							                                 #Assigning floating point number to isbn
+  @price = price					                               	 	 #Assigining floating point number to price
+  end
+ 
